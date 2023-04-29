3ccf27bd466786c00e74a7ce48056b54218c6b16
A8DE <abdssamad.gourchi@gmail.com>
2023-04-29 01:25:25 -0700

    Update

--- write_handlers.c
+++ write_handlers.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 /************************* WRITE HANDLE *************************/
@@ -251,6 +250,4 @@ int write_pointer(char buffer[], int ind, int length,
 		buffer[--ind] = extra_c;
 	return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
 }
-
-
 
